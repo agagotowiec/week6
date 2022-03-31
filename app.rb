485de9bbb4fe68e57c78f973ab717a2af807e3d3
agagotowiec <agnieszka.gtwc@gmail.com>
2022-03-31 13:32:30 +0100

    added deleting bookmarks fuctionality

--- app.rb
+++ app.rb
@@ -3,23 +3,30 @@ require 'sinatra/reloader'
 require_relative './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions, :method_override
+
     get '/' do
       'Bookmark Manager'
     end
 
     get '/bookmarks' do
       @bookmarks = Bookmark.all
-      erb :"bookmarks/index"
+      erb :'bookmarks/index'
     end
 
     get '/bookmarks/new' do
-      erb :"bookmarks/new"
+      erb :'bookmarks/new'
     end
 
     post '/bookmarks' do
       Bookmark.create(url: params[:url], title: params[:title])
       redirect '/bookmarks'
     end
-
+    
+    delete '/bookmarks/:id' do
+      Bookmark.delete(id: params[:id])
+      redirect '/bookmarks'
+    end
+  
     run! if app_file == $0
   end
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,25 +1,27 @@
 require 'pg'
 
 class Bookmark
-  
+
   attr_reader :id, :title, :url
 
   def initialize(id:, title:, url:)
+    @id  = id
     @id = id
     @title = title
     @url = url
   end
-  
+
   def self.all
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
-     result = connection.exec('SELECT * FROM bookmarks;')
-     result.map do |bookmark|
+
+      bookmarks = connection.exec('SELECT * FROM bookmarks;')
+      bookmarks.map do |bookmark|
       Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
-   end
+    end
   end
 
    def self.create(url:, title:)
@@ -28,8 +30,20 @@ class Bookmark
      else
        connection = PG.connect(dbname: 'bookmark_manager')
      end
-
-    result = connection.exec_params("INSERT INTO bookmarks (url, title) VALUES($1, $2) RETURNING id, title, url;", [url, title])
-    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+       result = connection.exec_params("INSERT INTO bookmarks (url, title) VALUES($1, $2) RETURNING id, title, url;", [url, title])
+       Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
    end
- end
+
+
+  def self.delete(id:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+    connection.exec_params("DELETE FROM bookmarks WHERE id = $1", [id])
+  end
+end
+
+
+  
